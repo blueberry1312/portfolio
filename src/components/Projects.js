ee10960a30067fc2857e514267c6aeb191fc912c
blueberry1312 <nestor.oro.alvarez@gmail.com>
2023-08-15 20:16:39 -0400

    Add about and contact

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import '../assets/style/projects.css';
 
 const Projects = () => {
   return (
-    <div id="projects">
+    <div className='projects-container' id="projects">
+      <h1 className='title subtitle'>PORTFOLIO</h1>
       <h1 className='title'>PROJECTS</h1>
-      <p>This is the projects section of my portfolio.</p>
+      <p className='resume'>Take a look at my projects:</p>
     </div>
   );
 };
